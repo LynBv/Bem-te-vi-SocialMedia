3807a54b38c42192221a3c75cd54d4a332ef3104
Elyn <elyndebrittob@gmail.com>
2024-11-03 16:11:33 -0300

    comentarios agora sao puxados da api

--- front-end/src/pages/Post/index.jsx
+++ front-end/src/pages/Post/index.jsx
@@ -1,63 +1,50 @@
-import React from "react";
-import Avatar from "../../assets/avatar.svg";
+import React, { useEffect, useState } from "react";
 import * as styles from "./Post.module.css";
 import { RiSendPlane2Fill } from "react-icons/ri";
 import Postagem from "../../components/Postagem";
 import { useParams } from "react-router-dom";
+import Comentario from "../../components/Comentario";
+import axios from "axios";
 
 export default function Post() {
   const { id } = useParams();
+  const [postagem, setPostagem] = useState({ usuarioNome: "", conteudo: "" });
+
+  const pegarPostagem = () => {
+    axios
+      .get(`http://localhost:8080/postagens/${id}`)
+      .then((response) => setPostagem(response.data))
+      .catch((error) => console.log(error))
+      .finally(() => console.log("acabou o get"));
+  };
+
+  useEffect(() => {
+    pegarPostagem();
+  }, []);
+
+  const montarComentario = postagem.comentarios?.map((comentario) => (
+      <Comentario key={comentario.id} comentario={comentario} />
+  ));
 
   return (
     <div className={styles.post}>
       <div className={styles.mainArea}>
         <div className={styles.PostArea}>
-          <Postagem id={id}/>
-          <div className={styles.comentar}>
-            <input
-              className={styles.comentarInput}
-              type="text"
-              name="comentario"
-              placeholder="deixe seu comentario..."
-            />
-            <button className={styles.comentarButton}>
-              <RiSendPlane2Fill />
-            </button>
-          </div>
+          <Postagem postagem={postagem} />
         </div>
-        <div className={styles.comentariosArea}>
-          <div className={styles.comentarioCard}>
-            <div className={styles.info}>
-              <img
-                src={Avatar}
-                alt="Foto de perfil"
-                className={styles.avatar}
-              />
-              <h1 className={styles.nomeUsuario}>Nome do usuario</h1>
-            </div>
-            <p className={styles.textoComentario}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
-              culpa tempore asperiores sunt quia veritatis soluta placeat quo
-              accusamus, fugit excepturi in est dolor voluptas, perspiciatis
-              numquam? Ad, natus rem.
-            </p>
-          </div>
-          <div className={styles.comentarioCard}>
-            <div className={styles.info}>
-              <img
-                src={Avatar}
-                alt="Foto de perfil"
-                className={styles.avatar}
-              />
-              <h1 className={styles.nomeUsuario}>Nome do usuario</h1>
-            </div>
-            <p className={styles.textoComentario}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
-              culpa tempore asperiores sunt quia veritatis soluta placeat quo
-              accusamus, fugit excepturi in est dolor voluptas, perspiciatis
-              numquam? Ad, natus rem.
-            </p>
-          </div>
+        <div className={styles.comentar}>
+          <input
+            className={styles.comentarInput}
+            type="text"
+            name="comentario"
+            placeholder="deixe seu comentario..."
+          />
+          <button className={styles.comentarButton}>
+            <RiSendPlane2Fill />
+          </button>
+        </div>
+         <div className={styles.comentariosArea}>
+          {montarComentario}
         </div>
       </div>
     </div>
--- front-end/src/routes/AppRoutes.jsx
+++ front-end/src/routes/AppRoutes.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Post from '../pages/Post'
-
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Post from "../pages/Post";
+import Login from "../pages/Login";
 
 export default function AppRoutes() {
   return (
-    <Routes>
-        <Route path='/post/:id' element={<Post/>}></Route>
-    </Routes>
-  )
+    <>
+      <Routes>
+        <Route path="/post/:id" element={<Post />}></Route>
+      </Routes>
+      <Routes>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </>
+  );
 }
