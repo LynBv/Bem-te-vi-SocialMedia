cb5057b826853d0c4c3a5c52679b3d45dedf62b1
Pedro Cancella Oliveira <pedrocancella94@gmail.com>
2024-11-05 21:39:35 -0300

    adcionando header

--- front-end/src/components/Header/index.jsx
+++ front-end/src/components/Header/index.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import React from "react";
 import * as styles from "../Header/Header.module.css";
+import { Link } from "react-router-dom";
+import Feed from "../../pages/Feed";
+import Login from "../../pages/Login";
+import Busca from "../../pages/Usuarios/index";
+import Logo from "../../assets/logo-bem-te-vi.png";
 
 export default function Header() {
   return (
     <div className={styles.header}>
+      <img src={Logo} alt="Logo" className={styles.logo} />
       <div className={styles.navBar}>
-
+        <div className={styles.navLinks}>
+          <Link
+            to={"/post/:id"}
+            element={<Feed />}
+            className={styles.opcaoNavBar}
+          >
+            Postagens
+          </Link>
+          <Link
+            to={"/login"}
+            element={<Login />}
+            className={styles.opcaoNavBar}
+          >
+            Login
+          </Link>
+          <Link
+            to={"/busca"}
+            element={<Busca />}
+            className={styles.opcaoNavBar}
+          >
+            Buscar
+          </Link>
+        </div>
       </div>
     </div>
-  )
+  );
 }
--- front-end/src/components/HeaderBusca/index.jsx
+++ front-end/src/components/HeaderBusca/index.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import * as styles from "../HeaderBusca/HeaderBusca.module.css";
 import Login from "../../pages/Login";
+import Feed from "../../pages/Feed";
+import Logo from "../../assets/logo-bem-te-vi.png";
 
 export default function HeaderBusca({
   handleSubmit,
@@ -9,9 +11,15 @@ export default function HeaderBusca({
 }) {
   return (
     <div className={styles.header}>
-      <div className={styles.navBar}></div>
-      {/* <Link to={"/"} element={<Feed />}></Link> */}
-      {/* <Link to={"/login"} element={<Login />}>Login</Link> */}
+      <img src={Logo} alt="Logo" className={styles.logo} />
+      <div className={styles.navBar}>
+        <Link to={"/post/:id"} element={<Feed />} className={styles.opcaoNavBar}>
+          Postagens
+        </Link>
+        <Link to={"/login"} element={<Login />} className={styles.opcaoNavBar}>
+          Login
+        </Link>
+      </div>
       <div className={styles.busca}>
         <form onSubmit={handleSubmit}>
           <input
